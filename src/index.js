7a775048a1c7178a24fe350ecfecb27af525aee4
Andrew Huth <andrew@huth.me>
2018-09-03 21:41:09 -0700

    hello world

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/app.jsx';
+
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
