c6c4ec76e83a5fff2096e4d7ae6df777f924b602
Andrew Huth <andrew@huth.me>
2018-09-07 18:23:39 -0700

    Just use a loop
    No need to be fancy

--- src/utils/get-teeth-path.js
+++ src/utils/get-teeth-path.js
@@ -1,15 +1,16 @@
-import range from 'lodash.range';
 import toCoordinates from './to-coordinates.js';
 
 const offset = 90;
 
 export default function getTeethPath(radius, angle, center, numTeeth) {
-  const commands = range(numTeeth + 1).map((num) => {
-    const a = angle * num - offset;
+  const commands = [];
+
+  for (let i = 0; i < numTeeth; i += 1) {
+    const a = angle * i - offset;
     const point = toCoordinates(radius, a, center);
-    const command = num === 0 ? 'M' : 'L';
-    return `${command} ${point.x} ${point.y}`;
-  });
+    const command = i === 0 ? 'M' : 'L';
+    commands.push(`${command} ${point.x} ${point.y}`);
+  }
 
   return commands.join(' ');
 }
