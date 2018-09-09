b79e2018aac61329f00314fcd5c1b6acefb0b9c3
Andrew Huth <andrew@huth.me>
2018-09-09 08:44:03 -0700

    Correct label associations
    Copy pasta :|

--- src/components/controls.jsx
+++ src/components/controls.jsx
@@ -15,11 +15,11 @@ export default function Controls({
         <input id="teeth" onChange={onTeethChange} min="3" max="24" type="range" value={teeth} />
       </div>
       <div>
-        <label htmlFor="teeth" style={styles.label}>Diameter 2 ({diameter2})</label>
+        <label htmlFor="d2" style={styles.label}>Diameter 2 ({diameter2})</label>
         <input id="d2" onChange={onDiameter2Change} min="0.25" max="1" step="0.03125" type="range" value={diameter2} />
       </div>
       <div>
-        <label htmlFor="teeth" style={styles.label}>Diameter 3 ({diameter3})</label>
+        <label htmlFor="d3" style={styles.label}>Diameter 3 ({diameter3})</label>
         <input id="d3" onChange={onDiameter3Change} min="0" max="0.75" step="0.03125" type="range" value={diameter3} />
       </div>
     </fieldset>
