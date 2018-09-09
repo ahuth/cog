77d204d47c185887bb96ce12781746bff0cbb83e
Andrew Huth <andrew@huth.me>
2018-09-09 08:43:27 -0700

    Tweak styling of the controls

--- src/components/controls.jsx
+++ src/components/controls.jsx
@@ -10,12 +10,24 @@ export default function Controls({
 }) {
   return (
     <fieldset>
-      <label htmlFor="teeth">Teeth ({teeth})</label>
-      <input id="teeth" onChange={onTeethChange} min="3" max="24" type="range" value={teeth} />
-      <label htmlFor="teeth">Diameter 2 ({diameter2})</label>
-      <input id="d2" onChange={onDiameter2Change} min="0.25" max="1" step="0.03125" type="range" value={diameter2} />
-      <label htmlFor="teeth">Diameter 3 ({diameter3})</label>
-      <input id="d3" onChange={onDiameter3Change} min="0" max="0.75" step="0.03125" type="range" value={diameter3} />
+      <div>
+        <label htmlFor="teeth" style={styles.label}>Teeth ({teeth})</label>
+        <input id="teeth" onChange={onTeethChange} min="3" max="24" type="range" value={teeth} />
+      </div>
+      <div>
+        <label htmlFor="teeth" style={styles.label}>Diameter 2 ({diameter2})</label>
+        <input id="d2" onChange={onDiameter2Change} min="0.25" max="1" step="0.03125" type="range" value={diameter2} />
+      </div>
+      <div>
+        <label htmlFor="teeth" style={styles.label}>Diameter 3 ({diameter3})</label>
+        <input id="d3" onChange={onDiameter3Change} min="0" max="0.75" step="0.03125" type="range" value={diameter3} />
+      </div>
     </fieldset>
   );
 }
+
+const styles = {
+  label: {
+    display: 'block',
+  },
+};
